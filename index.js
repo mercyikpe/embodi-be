255f6493afdd73a63498e16abd5586c7809314c6
donnwilzy <ask4godswill@gmail.com>
2023-07-27 15:23:40 +0100

    user model added

--- index.js
+++ index.js
@@ -10,11 +10,32 @@ const prodUrl = `http://127.0.0.1:${PORT}`
 const liveUrl =  `${process.env.currentUrl}:${PORT}`
 const currentUrl = liveUrl ||  prodUrl  
 
+//// DATABSE URL local: process.env.MONGODB_URI ||| cloud:process.env.databaseUrl 
+const dbUrl = process.env.databaseUrl || process.env.MONGODB_URI
+
+
+// Database connection
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+/// db connection error
+db.on('error', (error) => {
+  console.error('Connection error:', error);
+});
+//// db connecrion status.Successful alert
+db.once('open', () => {
+  console.log('Connection to MongoDB successful!');
+});
+//// db connecrion status.failure  alert
+db.once('close', () => {
+  console.log('Connection to MongoDB disconnected.');
+});
 
 
 app.get('/', (req, res) =>{
     res.send( `connected via ${currentUrl}`)
 })
+
+
 
 
 app.listen(PORT, ()=>{
---
+++ models/User.js
@@ -0,0 +1,46 @@
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isDoctor: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
