bd02bc577c4c89c75fc58ac04243e61377fc7df8
donnwilzy <ask4godswill@gmail.com>
2023-08-05 22:58:35 +0100

    user model updated

--- models/User.js
+++ models/User.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema(
   {
+    name: {
+      type: String,
+      //required: true,
+    },
+
     firstName: {
       type: String,
       //required: true,
